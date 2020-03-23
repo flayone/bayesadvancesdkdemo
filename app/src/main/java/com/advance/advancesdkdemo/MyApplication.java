d0c9c44521eaecd6d7eb7d1ddd7e955d5b65631d
Aaron <406505223@qq.com>
2020-03-23 18:00:47 +0800

    基于3,0,7版本改动

--- app/src/main/java/com/advance/advancesdkdemo/MyApplication.java
+++ app/src/main/java/com/advance/advancesdkdemo/MyApplication.java
@@ -3,6 +3,7 @@ package com.advance.advancesdkdemo;
 import android.app.Application;
 
 import com.advance.AdvanceConfig;
+import com.bytedance.sdk.openadsdk.TTAdConstant;
 
 public class MyApplication extends Application {
     @Override
@@ -10,5 +11,15 @@ public class MyApplication extends Application {
         super.onCreate();
         //聚合SDK初始化，请在bugly等sdk初始化之前
         AdvanceConfig.getInstance().setOaid("").setDebug(true).initSDKs(this);
+
+        //设置穿山甲允许直接下载的网络状态集合，可以在调用广告之前的任何时间来设置
+        int[] directDownloadNetworkType;
+        boolean csjDownloadForWifi = true;
+        if (csjDownloadForWifi) {//可以根据自己需求来设置状态值
+            directDownloadNetworkType = new int[]{TTAdConstant.NETWORK_STATE_WIFI};
+        } else {
+            directDownloadNetworkType = new int[]{TTAdConstant.NETWORK_STATE_4G};
+        }
+        AdvanceConfig.getInstance().setCsjDirectDownloadNetworkType(directDownloadNetworkType);
     }
 }
