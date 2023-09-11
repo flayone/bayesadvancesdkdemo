5bb8656ff4405ebccb2bc84162eefeaf8081c503
Aaron <406505223@qq.com>
2023-09-11 11:48:41 +0800

    mry ----> 402

--- app/src/main/java/com/advance/advancesdkdemo/Constants.java
+++ app/src/main/java/com/advance/advancesdkdemo/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
         //以下测试id默认配置了穿山甲广告
         public static final String splashAdspotId = "10003083"; //开屏
         public static final String bannerAdspotId = "10003091"; //banner
-        public static final String interstitialAdspotId = "10008264"; //插屏
+        public static final String interstitialAdspotId = "10003097"; //插屏
         public static final String nativeExpressAdspotId = "10003094"; //原生模板信息流
         public static final String rewardAdspotId = "10003100"; //激励视频
         public static final String fullScreenVideoAdspotId = "10003103"; //全屏视频
