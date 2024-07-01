901c87658bb9b80588053321b83f9638e1b05ada
Aaron <406505223@qq.com>
2024-07-01 16:37:49 +0800

    ttt

--- app/src/main/java/com/advance/advancesdkdemo/Constants.java
+++ app/src/main/java/com/advance/advancesdkdemo/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     //示例代码位id（仅测试用，不可用来上线正式使用！），具体展现的是哪个广告, 由后台策略来决定。实际使用请使用自己的广告位id，一定不要使用这里的测试id上线发布，否则无收益！
     public static class TestIds {
         //以下测试id默认配置了穿山甲广告
-//        public static final String splashAdspotId = "10007771"; //开屏
-        public static final String splashAdspotId = "10009581"; //人人开屏bidding
+        public static final String splashAdspotId = "10007771"; //开屏
+//        public static final String splashAdspotId = "10009581"; //人人开屏bidding
         public static final String bannerAdspotId = "10007795"; //banner
         public static final String interstitialAdspotId = "10007796"; //插屏
         public static final String nativeExpressAdspotId = "10007794"; //原生模板信息流
